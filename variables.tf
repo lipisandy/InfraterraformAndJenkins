e0189a092b516c4dfa4036154d23a07539404da5
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-29 13:06:47 +0530

    Update variables.tf
    variable changes

--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
 variable "aws_region" {
        description = "The AWS region to create things in." 
-       default     = "ap-south-1" 
+       default     = "us-east-1" 
 }
 
 variable "key_name" { 
     description = " SSH keys to connect to ec2 instance" 
-    default     =  "HashTek_domain" 
+    default     =  "n.virginia.pem" 
 }
 
 variable "instance_type" { 
     description = "instance type for ec2" 
-    default     =  "t2.micro" 
+    default     =  "t2.medium" 
 }
 
 variable "security_group" { 
     description = "Name of security group" 
-    default     = "jenkins-sgroup-dec-2021" 
+    default     = "sg-040fb58030c59597e"
 }
 
 variable "tag_name" { 
     description = "Tag Name of for Ec2 instance" 
-    default     = "my-ec2-instance" 
+    default     = "east1-ec2" 
 } 
 variable "ami_id" { 
-    description = "AMI for Ubuntu Ec2 instance" 
-    default     = "ami-062df10d14676e201" 
+    description = "AMI for linux Ec2 instance" 
+    default     = "ami-051f7e7f6c2f40dc1" 
 }
 variable "versioning" {
     type        = bool
