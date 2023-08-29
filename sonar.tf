3f90ed6dda65782b23845900b2fb26b182cfa8b9
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-29 13:41:20 +0530

    Update sonar.tf
    security group and ami changes

--- sonar.tf
+++ sonar.tf
@@ -1,15 +1,15 @@
 resource "aws_instance" "mySonarInstance" {
-      ami           = "ami-0e6329e222e662a52"
+      ami           = "ami-051f7e7f6c2f40dc1"
       key_name = var.key_name
-      instance_type = "t2.micro"
-      vpc_security_group_ids = [aws_security_group.sonar-sg-2022.id]
+      instance_type = "t2.medium"
+      vpc_security_group_ids = [aws_security_group.sg-0cf7e6b3bbb343ed3]
       tags= {
         Name = "sonar_instance"
       }
     }
 
  resource "aws_security_group" "sonar-sg-2022" {
-      name        = "security_sonar_group_2022"
+      name        = "sg-0cf7e6b3bbb343ed3"
       description = "security group for Sonar"
 
       ingress {
@@ -42,7 +42,7 @@ resource "aws_instance" "mySonarInstance" {
 # Create Elastic IP address for Sonar instance
 resource "aws_eip" "mySonarInstance" {
   vpc      = true
-  instance = aws_instance.mySonarInstance.id
+  instance = aws_instance.mySonarInstance.eipalloc-065e8ce1c4cd09b35
 tags= {
     Name = "sonar_elastic_ip"
   }
