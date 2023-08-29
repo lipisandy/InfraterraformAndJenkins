954b34da8c92b9366b3f5514a23a39ebfae0fa87
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-29 18:58:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_instance" "myFirstInstance" {
   ami           = var.ami_id
   key_name = var.key_name
   instance_type = var.instance_type
-  vpc_security_group_ids = [var.security-group]
+  vpc_security_group_ids = [var.security_group]
   tags= {
     Name = var.tag_name
   }
