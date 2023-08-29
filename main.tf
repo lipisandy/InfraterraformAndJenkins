d2b5acdcf5283269763f6703e4af1698b3037693
lipisandy <131548032+lipisandy@users.noreply.github.com>
2023-08-29 19:01:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -55,7 +55,7 @@ resource "aws_instance" "myFirstInstance" {
 # Create Elastic IP address
 resource "aws_eip" "myFirstInstance" {
   vpc      = true
-  instance = aws_instance.myFirstInstance.eipalloc-065e8ce1c4cd09b35
+  instance = aws_instance.myFirstInstance.id
 tags= {
     Name = "my_elastic_ip"
   }
